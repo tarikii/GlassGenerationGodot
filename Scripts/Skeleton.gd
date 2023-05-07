528856331a3f9a73075b3469af39c63854b64fc3
tarikii <aabouchazougarthtarik@elpuig.xeill.net>
2023-05-08 01:59:07 +0200

    Mitad de las animaciones hechas

--- Scripts/Skeleton.gd
+++ Scripts/Skeleton.gd
@@ -2,10 +2,10 @@ extends KinematicBody2D
 
 const SPEED = 100
 var motion = Vector2()
-onready var animation = $KnightPlayer
+onready var animation = $SkeletonPlayer
 
 func _physics_process(delta):
 	motion = move_and_slide(motion)
 
 func _process(delta):
-	animation.play("Attack Left")
+	animation.play("Attack Right")
