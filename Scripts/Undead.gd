377a5bf06b68b56781fd6379be43ff2bc405c642
tarikii <aabouchazougarthtarik@elpuig.xeill.net>
2023-05-09 02:09:22 +0200

    animaciones hechas

--- Scripts/Undead.gd
+++ Scripts/Undead.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 const SPEED = 100
 var motion = Vector2()
-onready var animation = $SkeletonPlayer
+onready var animation = $UndeadPlayer
 
 func _physics_process(delta):
 	motion = move_and_slide(motion)
---
+++ Scripts/Witch.gd
@@ -0,0 +1,11 @@
+extends KinematicBody2D
+
+const SPEED = 100
+var motion = Vector2()
+onready var animation = $WitchPlayer
+
+func _physics_process(delta):
+	motion = move_and_slide(motion)
+
+func _process(delta):
+	animation.play("Attack Right")
