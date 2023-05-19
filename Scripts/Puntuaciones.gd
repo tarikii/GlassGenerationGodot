4517dfc173441b35e0ae8dd7e026fa4d63c90e0c
tarikii <aabouchazougarthtarik@elpuig.xeill.net>
2023-05-19 17:51:03 +0200

    Escena instrucciones añadida

--- Scripts/Puntuaciones.gd
+++ Scripts/Puntuaciones.gd
@@ -1,23 +1,28 @@
-extends Control
+extends Node2D
 var escenaMenu
+var primerLabelLleno = false
 
 func _ready():
-	if $Fondomenu/ColorRect/VBoxNicknames/Nickname.text == "":
-		$Fondomenu/ColorRect/VBoxNicknames/Nickname.text = Nickname.nickname
-	else:
-		pass
-			
-	if $Fondomenu/ColorRect/VBoxNicknames/Nickname2.text == "":
-		$Fondomenu/ColorRect/VBoxNicknames/Nickname2.text = Nickname.nickname
-	else:
-		pass
-			
-	if Result.result != null:
-		$Fondomenu/ColorRect/VBoxResult/Result.text = Result.result
-		
-	$Fondomenu/ColorRect/VBoxPoints/Points.text = str(Points.points)
+	$Fondomenu/ColorRect/VBoxNicknames/Nickname.text = ""
+	$Fondomenu/ColorRect/VBoxNicknames/Nickname2.text = ""
+	rellenarPuntuaciones()
 
 func volverAlMenu():
 	escenaMenu = "res://Scenes/Menu.tscn"
 	get_tree().change_scene(escenaMenu)
+
+func rellenarPuntuaciones():
+	if Nickname.nickname == null:
+		pass
+	else:
+		var nickname = Nickname.nickname
+		var label1 = $Fondomenu/ColorRect/VBoxNicknames/Nickname
+		var label2 = $Fondomenu/ColorRect/VBoxNicknames/Nickname2
+
+		if label1.text == "":
+			label1.text = nickname
+		elif label2.text == "":
+			label2.text = nickname
+		else:
+			label1.text = nickname
 
